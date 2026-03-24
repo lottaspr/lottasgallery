117be7f60ebee45062471713b24a0dc9e195f962
lottaspr <lotta.spring@remhaug.de>
2026-03-24 14:01:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.style.display = "none");
+    slides[index].style.display = "block";
+}
+
+document.getElementById("next").addEventListener("click", () => {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+});
+
+showSlide(current);
